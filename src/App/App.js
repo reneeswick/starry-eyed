d7699dfb0589ae42a0dc662b2c92e8ed7b8e8f97
Renee Swick-Ziller <reneeswick@gmail.com>
2021-11-10 08:36:34 -0700

    Add Routes and NavLinks

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import '../index.css';
 import BlogCardContainer from '../BlogCardContainer/BlogCardContainer';
 import SavedForLaterContainer from '../SavedForLaterContainer/SavedForLaterContainer';
 import ISSReportContainer from '../ISSReportContainer/ISSReportContainer';
@@ -11,12 +12,18 @@ const App = () => {
   return (
     <main>
       <h1>Starry Eyed</h1>
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/blogs'>Blogs</NavLink>
+        <NavLink to='/saved'>Saved for Later</NavLink>
+        <NavLink to='/report'>ISS Report</NavLink>
+      </nav>
       <Switch>
-        <Home />
-        <BlogCardContainer />
-        <SavedForLaterContainer />
-        <ISSReportContainer />
-        <Error />
+        <Route exact path='/' render={() => <Home />} />
+        <Route path='/blogs' render={() => <BlogCardContainer />} />
+        <Route path='/saved' render={() => <SavedForLaterContainer />} />
+        <Route path='/report' render={() => <ISSReportContainer />} />
+        <Route path='/error' render={() => <Error />} />
       </Switch>
     </main>
   )
