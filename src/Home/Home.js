dd6ffc7b1e3ef4839241d55cc34e7c761d88e318
Renee Swick-Ziller <reneeswick@gmail.com>
2021-11-14 10:22:46 -0700

    styled header border bottom

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './Home.css';
 
-const Home = ({ changeToDarkMode }) => {
+const Home = () => {
 
   return (
     <div className='home-view'>
       <h2 className='slogan'>Feeling Starry Eyed?</h2>
       <p className='about-us'>Check out our space blog, save some articles for later, or track down the latest news from the International Space Station (ISS)</p>
-      {/*<p className='prompt'>Choose your mode</p>*/}
-      {/*<div className='mode-container'>
-        <p>☀️</p>
-        <label class='switch'>
-          <input
-            type='checkbox'
-            name='mode'
-            onClick={(event) => changeToDarkMode(event)}
-          />
-          <span class='slider round'></span>
-        </label>
-        <p>🌙</p>
-      </div>*/}
     </div>
   )
 }
